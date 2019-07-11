5f646bc8f7ba8a0564357eab6d1f8d541d8663c4
feiyuzhu <feiyu.zhu@uwaterloo.ca>
2019-07-11 15:28:12 -0400

    renamed the argument

--- R/psdSHO.R
+++ R/psdSHO.R
@@ -5,9 +5,9 @@
 #' @param k Cantilever stiffness.
 #' @param Kb Boltzmann constant.
 #' @param T Temperature in Kelvin.
-#' @param convert Indicator of whether or not to convert the unit of PSD to fm2/Hz. The femtometer (fm) is an SI unit of length equal to 1e-15 meters.
+#' @param unit_conversion Indicator of whether or not to convert the unit of PSD to fm2/Hz. The femtometer (fm) is an SI unit of length equal to 1e-15 meters.
 #' @export
-psdSHO <- function(f, f0, Q, k, Kb, T, convert = TRUE) {
+psdSHO <- function(f, f0, Q, k, Kb, T, unit_conversion = TRUE) {
   numerator <- Kb * T / (k * pi * f0 * Q)
   denominator <- ((f/f0)^2 - 1)^2 + (f/(f0*Q))^2
   psd <- numerator/denominator
