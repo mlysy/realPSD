45496007c8457cdfd98877d06d7d0a431bcd12c3
feiyuzhu <feiyu.zhu@uwaterloo.ca>
2019-07-16 16:53:29 -0400

    tested plot2 on a single core

--- tests/dontrun/plot2.R
+++ tests/dontrun/plot2.R
@@ -1,6 +1,7 @@
 # to reproduce Figure 2 in the paper
 require(realPSD)
 require(TMB)
+require(tidyverse)
 # ---------- SHO model parameters ----------
 T  <- 5                   # Total time, second
 fs <- 1e7                 # Sampling frequency, Hz
@@ -27,7 +28,7 @@ f <- seq(from = f_lb, to = f_ub, by = 1/T) # frequency domain, Hz
 # for loop
 nsim <- 10
 # pre-allocate space for storage
-fit_Q1_lp <- matrix(NA, nsim, 3) 
+fit_Q1_lp <- matrix(NA, nsim, 3)
 fit_Q1_nls <- matrix(NA, nsim, 3) 
 fit_Q1_mle <- matrix(NA, nsim, 3) 
 fit_Q10_lp <- matrix(NA, nsim, 3) 
@@ -39,38 +40,43 @@ fit_Q100_mle <- matrix(NA, nsim, 3)
 fit_Q500_lp <- matrix(NA, nsim, 3) 
 fit_Q500_nls <- matrix(NA, nsim, 3) 
 fit_Q500_mle <- matrix(NA, nsim, 3) 
+# specify the Q factor values
+Q1 <- Q_vec[1] 
+Q10 <- Q_vec[2]
+Q100 <- Q_vec[3]
+Q500 <- Q_vec[4]
 system.time(
 for(ii in 1:nsim) {
   # generate a vector of exponential random variables 
   # which is the same for all Q1, Q10, Q100, Q500 but different for each iteration/simulation 
   rfreq <- rexp(length(f), rate = 1) 
   # ----- fitting simulated data -----
-  # specify the Q factor
-  Q1 <- Q_vec[1] 
+  # # specify the Q factor
+  # Q1 <- Q_vec[1] 
   fit_Q1_lp[ii, ] <- fitSHOW(f, rfreq, fs, f0, Q1, k, T, Aw, 
     binSize = 100, method = "LP_nlp")  # log-periodogram
   fit_Q1_nls[ii, ] <- fitSHOW(f, rfreq, fs, f0, Q1, k, T, Aw, 
     binSize = 100, method = "NLS_nlp") # Nonlinear least-squares
   fit_Q1_mle[ii, ] <- fitSHOW(f, rfreq, fs, f0, Q1, k, T, Aw, 
     binSize = 100, method = "MLE_nlp") # Whittle likelihood
-  # specify the Q factor
-  Q10 <- Q_vec[2]
+  # # specify the Q factor
+  # Q10 <- Q_vec[2]
   fit_Q10_lp[ii, ] <- fitSHOW(f, rfreq, fs, f0, Q10, k, T, Aw, 
     binSize = 100, method = "LP_nlp")  # log-periodogram
   fit_Q10_nls[ii, ] <- fitSHOW(f, rfreq, fs, f0, Q10, k, T, Aw, 
     binSize = 100, method = "NLS_nlp") # Nonlinear least-squares
   fit_Q10_mle[ii, ] <- fitSHOW(f, rfreq, fs, f0, Q10, k, T, Aw, 
     binSize = 100, method = "MLE_nlp") # Whittle likelihood  
-  # specify the Q factor
-  Q100 <- Q_vec[3]
+  # # specify the Q factor
+  # Q100 <- Q_vec[3]
   fit_Q100_lp[ii, ] <- fitSHOW(f, rfreq, fs, f0, Q100, k, T, Aw, 
     binSize = 100, method = "LP_nlp")  # log-periodogram
   fit_Q100_nls[ii, ] <- fitSHOW(f, rfreq, fs, f0, Q100, k, T, Aw, 
     binSize = 100, method = "NLS_nlp") # Nonlinear least-squares
   fit_Q100_mle[ii, ] <- fitSHOW(f, rfreq, fs, f0, Q100, k, T, Aw, 
     binSize = 100, method = "MLE_nlp") # Whittle likelihood  
-  # specify the Q factor
-  Q500 <- Q_vec[4]
+  # # specify the Q factor
+  # Q500 <- Q_vec[4]
   fit_Q500_lp[ii, ] <- fitSHOW(f, rfreq, fs, f0, Q500, k, T, Aw, 
     binSize = 100, method = "LP_nlp")  # log-periodogram
   fit_Q500_nls[ii, ] <- fitSHOW(f, rfreq, fs, f0, Q500, k, T, Aw, 
@@ -79,6 +85,69 @@ for(ii in 1:nsim) {
     binSize = 100, method = "MLE_nlp") # Whittle likelihood  
 }
 )
+# covnert the estimated parameters to ratios
+# Q = 1
+fit_Q1_lp <- fit_Q1_lp %*% diag(c(1/f0, 1/Q1, 1/k))
+fit_Q1_nls <- fit_Q1_nls %*% diag(c(1/f0, 1/Q1, 1/k))
+fit_Q1_mle <- fit_Q1_mle %*% diag(c(1/f0, 1/Q1, 1/k))
+colnames(fit_Q1_lp) <- c("f0", "Q", "k")
+colnames(fit_Q1_nls) <- c("f0", "Q", "k")
+colnames(fit_Q1_mle) <- c("f0", "Q", "k")
+# Q = 10
+fit_Q10_lp <- fit_Q10_lp %*% diag(c(1/f0, 1/Q10, 1/k))
+fit_Q10_nls <- fit_Q10_nls %*% diag(c(1/f0, 1/Q10, 1/k))
+fit_Q10_mle <- fit_Q10_mle %*% diag(c(1/f0, 1/Q10, 1/k))
+colnames(fit_Q10_lp) <- c("f0", "Q", "k")
+colnames(fit_Q10_nls) <- c("f0", "Q", "k")
+colnames(fit_Q10_mle) <- c("f0", "Q", "k")
+# Q = 100
+fit_Q100_lp <- fit_Q100_lp %*% diag(c(1/f0, 1/Q100, 1/k))
+fit_Q100_nls <- fit_Q100_nls %*% diag(c(1/f0, 1/Q100, 1/k))
+fit_Q100_mle <- fit_Q100_mle %*% diag(c(1/f0, 1/Q100, 1/k))
+colnames(fit_Q100_lp) <- c("f0", "Q", "k")
+colnames(fit_Q100_nls) <- c("f0", "Q", "k")
+colnames(fit_Q100_mle) <- c("f0", "Q", "k")
+# Q = 500
+fit_Q500_lp <- fit_Q500_lp %*% diag(c(1/f0, 1/Q500, 1/k))
+fit_Q500_nls <- fit_Q500_nls %*% diag(c(1/f0, 1/Q500, 1/k))
+fit_Q500_mle <- fit_Q500_mle %*% diag(c(1/f0, 1/Q500, 1/k))
+colnames(fit_Q500_lp) <- c("f0", "Q", "k")
+colnames(fit_Q500_nls) <- c("f0", "Q", "k")
+colnames(fit_Q500_mle) <- c("f0", "Q", "k")
+# merge datasets
+# Q = 1
+fit_Q1_lp <- as_tibble(fit_Q1_lp) %>% add_column(method = "LP")
+fit_Q1_nls <- as_tibble(fit_Q1_nls) %>% add_column(method = "NLS")
+fit_Q1_mle <- as_tibble(fit_Q1_mle) %>% add_column(method = "MLE")
+fit_Q1 <- bind_rows(fit_Q1_lp, fit_Q1_nls, fit_Q1_mle)
+fit_Q1 <- fit_Q1 %>% add_column(level = "Q = 1")
+# Q = 10
+fit_Q10_lp <- as_tibble(fit_Q10_lp) %>% add_column(method = "LP")
+fit_Q10_nls <- as_tibble(fit_Q10_nls) %>% add_column(method = "NLS")
+fit_Q10_mle <- as_tibble(fit_Q10_mle) %>% add_column(method = "MLE")
+fit_Q10 <- bind_rows(fit_Q10_lp, fit_Q10_nls, fit_Q10_mle) %>% 
+  add_column(level = "Q = 10")
+# Q = 100
+fit_Q100_lp <- as_tibble(fit_Q100_lp) %>% add_column(method = "LP")
+fit_Q100_nls <- as_tibble(fit_Q100_nls) %>% add_column(method = "NLS")
+fit_Q100_mle <- as_tibble(fit_Q100_mle) %>% add_column(method = "MLE")
+fit_Q100 <- bind_rows(fit_Q100_lp, fit_Q100_nls, fit_Q100_mle) %>% 
+  add_column(level = "Q = 100")
+# Q = 500
+fit_Q500_lp <- as_tibble(fit_Q500_lp) %>% add_column(method = "LP")
+fit_Q500_nls <- as_tibble(fit_Q500_nls) %>% add_column(method = "NLS")
+fit_Q500_mle <- as_tibble(fit_Q500_mle) %>% add_column(method = "MLE")
+fit_Q500 <- bind_rows(fit_Q500_lp, fit_Q500_nls, fit_Q500_mle) %>% 
+  add_column(level = "Q = 500")
+# combine all the datasets together
+fit <- bind_rows(fit_Q1, fit_Q10, fit_Q100, fit_Q500)
+# boxplot
+# Q_hat / Q
+ggplot(fit, aes(x = level, y = Q, fill = method)) + geom_boxplot()
+# k_hat / k
+ggplot(fit, aes(x = level, y = k, fill = method)) + geom_boxplot()
+# f0_hat / f0
+ggplot(fit, aes(x = level, y = f0, fill = method)) + geom_boxplot()
 
 
 # nbins <- ceiling(length(f) / binSize)
