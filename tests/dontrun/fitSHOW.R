b3c50ad3dd518edcdce95588cd03aad0b5885cd9
feiyuzhu <feiyu.zhu@uwaterloo.ca>
2019-07-17 22:01:32 -0400

    applied the profile likelihood correctly

--- tests/dontrun/fitSHOW.R
+++ tests/dontrun/fitSHOW.R
@@ -1,4 +1,5 @@
-#' @title fit simulated datasets to get fitted parameters
+#' @title Fit simulated datasets to get fitted parameters (for internal use)
+#' 
 #' @param fseq Sequence of frequencies, usually from f0 - f0/sqrt(2) to f0 + f0/sqrt(2).
 #' @param sim_exp Vector of exponential random variables Exp(1) with the same length as fseq.
 #' @param fs Sampling frequency, Hz.
@@ -9,22 +10,22 @@
 #' @param Aw White noise psd.
 #' @param bin_size Integer number, bin size.
 #' @param method Fitting method, i.e. lp, mle, nls.
-#' @export
 fitSHOW <- function(fseq, sim_exp, fs, f0, Q, k, Temp, Aw,
                     bin_size = 100, method = c("lp", "mle", "nls")) {
+  # ---------- setup -----------
   method <- match.arg(method)
   Kb <- 1.381e-23           # Boltzmann's constant
-  sig2 <- Kb*T/(k*pi*f0*Q) # variance
+  sig2 <- Kb*Temp/(k*pi*f0*Q) # variance
   Rw <- Aw/sig2 # re-parameterization
   phi <- c(f0, f0*Q, Rw) # parameter vector for SHOW model
   # psd values at each frequency point of f with given Q
-  psd <- psdSHO(f, f0, Q, k, Kb, T, unit_conversion = TRUE) + Aw
+  psd <- psdSHO(fseq, f0, Q, k, Kb, Temp, unit_conversion = TRUE) + Aw
   # generate the periodogram values
-  Y <- rfreq * fs * psd
+  Y <- sim_exp * fs * psd
   # ---------- binning ----------
-  # binSize <- 100
-  fbar <- binning(f, binSize = binSize)
-  Ybar <- binning(Y, binSize = binSize)
+  # bin_size <- 100
+  fbar <- binning(fseq, bin_size = bin_size)
+  Ybar <- binning(Y, bin_size = bin_size)
   Zbar <- log(Ybar)
   # ---------- fitting ----------
   if (method == "lp") {
@@ -81,13 +82,13 @@ fitSHOW <- function(fseq, sim_exp, fs, f0, Q, k, Temp, Aw,
   }
   opt <- optim(phi, fn = obj$fn, gr = obj$gr,
                control = list(maxit = 1000))
-  phi_hat <- opt$par # extract the fitted parameters
+  phi_hat <- opt$par # extract the fitted phi = c(f0_hat, gamma_hat, Rw_hat)
   tau_hat <- get_tau(phi_hat) # fitted tau = sigma^2
   param <- rep(NA, 4) # allocate space for storage
   param[1] <- phi_hat[1] # f0_hat
   param[2] <- phi_hat[2]/phi_hat[1] # Q_hat
-  param[3] <- phi_hat[3] / Aw * Kb * T / (pi * phi_hat[2]) # k_hat
-  # param[4] = ???
+  param[3] <- Kb * Temp / (tau_hat * pi * phi_hat[2]) # k_hat
+  param[4] <- phi_hat[3] * tau_hat # Aw_hat
   names(param) <- c("f0", "Q", "k", "Aw")
   return(param)
 }
