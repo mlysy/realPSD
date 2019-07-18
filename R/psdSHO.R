c04ab1f62f62545c8a47fa4e25fc5c6eb3e27e9c
feiyuzhu <feiyu.zhu@uwaterloo.ca>
2019-07-17 21:51:42 -0400

    modified the argument name and the function body accordingly

--- R/psdSHO.R
+++ R/psdSHO.R
@@ -1,5 +1,5 @@
 #' @title Power spectral density of SHO model.
-#' @param f Frequency or a vector of frequencies in Hz.
+#' @param fseq Frequency or a vector of frequencies in Hz.
 #' @param f_0 Resonance frequency f_0.
 #' @param Q Quality factor.
 #' @param k Cantilever stiffness.
@@ -7,9 +7,9 @@
 #' @param Temp Temperature in Kelvin.
 #' @param unit_conversion Indicator of whether or not to convert the unit of PSD to fm2/Hz. The femtometer (fm) is an SI unit of length equal to 1e-15 meters.
 #' @export
-psdSHO <- function(f, f0, Q, k, Kb, Temp, unit_conversion = TRUE) {
-  numerator <- Kb * T / (k * pi * f0 * Q)
-  denominator <- ((f/f0)^2 - 1)^2 + (f/(f0*Q))^2
+psdSHO <- function(fseq, f0, Q, k, Kb, Temp, unit_conversion = TRUE) {
+  numerator <- Kb * Temp / (k * pi * f0 * Q)
+  denominator <- ((fseq/f0)^2 - 1)^2 + (fseq/(f0*Q))^2
   psd <- numerator/denominator
   CONSTANT <- 1
   if(unit_conversion == TRUE) CONSTANT = 1e30
