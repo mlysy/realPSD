5336487eb47178d83fb30405187c945af6b01480
feiyuzhu <feiyu.zhu@uwaterloo.ca>
2019-07-23 23:10:40 -0400

    tested code for data manipulation after simulation and plotting boxplot

--- tests/dontrun/plot2_parallel.R
+++ tests/dontrun/plot2_parallel.R
@@ -1,14 +1,16 @@
 # to reproduce Figure 2 in the paper
 require(realPSD)
 # require(TMB)
-require(tidyverse)
+# require(tidyverse)
 require(parallel)
 # TODO: factor out an exportable function show_fit
 # and perhaps a non-exported function show_fsim (f is for freq)
 source("fitSHOW.R")
 # data folder
-data_path_sim <- "~/Documents/data/R/realPSD/show_sim"
-data_path_fit <- "~/Documents/data/R/realPSD/show_fit"
+# data_path_sim <- "~/Documents/data/R/realPSD/show_sim"
+data_path_sim <- "~/realPSD/show_sim"
+# data_path_fit <- "~/Documents/data/R/realPSD/show_fit"
+data_path_fit <- "~/realPSD/show_fit"
 # clear any existing files
 unlink(file.path(data_path_sim, "*"), recursive = TRUE)
 unlink(file.path(data_path_fit, "*"), recursive = TRUE)
@@ -33,7 +35,7 @@ fseq <- seq(from = f_lb, to = f_ub, by = 1/Time) # frequency domain, Hz
 nf <- length(fseq) # number of frequencies
 
 # ---------- simulation ----------
-nsim <- 10
+nsim <- 1000
 bin_size <- 100
 
 # first, pregenerate exponentials
@@ -50,12 +52,17 @@ if(sim_expo) {
 # this is for illustrative purposes mainly.
 # i would probably do all the Q's and methods in the same loop,
 # i.e., loop over only the datasets
-fit_descr <- expand.grid(Q = Q_vec,
+fit_descr <- expand.grid(Q_level = Q_vec,
                         method = c("lp", "nls", "mle"),
                         data_id = 1:nsim,
-                        stringsAsFactors = FALSE) %>% as_tibble()
+                        stringsAsFactors = FALSE)
 nfit <- nrow(fit_descr)
 
+# detect the number of cores
+ncores <- detectCores()
+# set seed for reproducibility
+set.seed(123, kind = "L'Ecuyer-CMRG")
+# run the simulation
 system.time(
 success <- mclapply(1:nfit, function(ii) {
   # multi-assign elements of job: data_id, Q, method
@@ -79,5 +86,42 @@ success <- mclapply(1:nfit, function(ii) {
     out <- TRUE
   } else out <- FALSE
   out
-})
+}, mc.cores = ncores)
 )
+# check if all iterations were successful
+all(success == TRUE)
+
+# ---------- Code below should be commented out for server computing ---------------
+# read simulated data into workspace
+for(ii in 1:nfit) {
+  assign(paste0("fit_", ii), 
+        readRDS(file.path(data_path_fit, paste0("show_fit_", ii, ".rds"))))
+}
+# combine each iteration vector into a data frame
+fit_list <- list()
+for(ii in 1:nfit) {
+  fit_list[[ii]] <- readRDS(file.path(data_path_fit, paste0("show_fit_", ii, ".rds"))) 
+}
+fit_data <- do.call(rbind, fit_list)
+fit_data <- cbind(fit_data, fit_descr[,c("Q_level", "method")])
+# then manipulate the data frame using tidyverse toolbox
+fit_data <- fit_data %>% as_tibble() %>%
+  mutate(Q_level = factor(Q_level,  # convert the column Q_level into a factor
+    levels = c(1,10,100,500), labels = c("Q = 1", "Q = 10", "Q = 100", "Q = 500"))) %>% 
+  mutate(method = factor(method, ordered = FALSE)) # factor column method 
+# get a new dataset with ratios instead of fitted values
+ratio_data <- fit_data %>% 
+  mutate(f0_hat = f0_hat/f0) %>%
+  mutate(k_hat = k_hat/k) %>%
+  mutate(Aw_hat = Aw_hat/Aw) %>%
+  mutate(Q_hat = ifelse(Q_level == "Q = 1", Q_hat/Q_vec[1], Q_hat)) %>%
+  mutate(Q_hat = ifelse(Q_level == "Q = 10", Q_hat/Q_vec[2], Q_hat)) %>%
+  mutate(Q_hat = ifelse(Q_level == "Q = 100", Q_hat/Q_vec[3], Q_hat)) %>%
+  mutate(Q_hat = ifelse(Q_level == "Q = 500", Q_hat/Q_vec[4], Q_hat))
+  # boxplot
+# Q_hat / Q
+ggplot(ratio_data, aes(x = Q_level, y = Q_hat, fill = method)) + geom_boxplot()
+# k_hat / k
+ggplot(ratio_data, aes(x = Q_level, y = k_hat, fill = method)) + geom_boxplot()
+# f0_hat / f0
+ggplot(ratio_data, aes(x = Q_level, y = f0_hat, fill = method)) + geom_boxplot()
--- tests/dontrun/test.R
+++ tests/dontrun/test.R
@@ -1,72 +1,105 @@
 # test
+install.packages("realPSD_1.0.tar.gz", type = "source", repos = NULL, INSTALL_opts = "--install-tests")
+require(tidyverse)
+require(parallel)
 # read fitted data
-tmp <- readRDS(file = file.path(data_path_fit, paste0("show_fit_", 1, ".rds")))
+data_path_fit <- "show_fit"
+# tmp <- readRDS(file = file.path(data_path_fit, paste0("show_fit_", 1, ".rds")))
+nfit <- nrow(fit_descr)
+system.time(
+for(ii in 1:nfit) {
+  assign(paste0("fit_", ii), 
+        readRDS(file.path(data_path_fit, paste0("show_fit_", ii, ".rds"))))
+}
+)
 
+fit_list <- list()
+system.time(
+for(ii in 1:nfit) {
+  fit_list[[ii]] <- readRDS(file.path(data_path_fit, paste0("show_fit_", ii, ".rds"))) 
+}
+)
 
+fit_data <- do.call(rbind, fit_list)
+fit_data <- cbind(fit_data, fit_descr[,c("Q_level", "method")])
 
-tmp <- readRDS("fit_Q1_lp_20.rds")
+# then manipulate the data frame using tidyverse toolbox
+fit_data <- fit_data %>% as_tibble() %>%
+  mutate(Q_level = factor(Q_level,  # convert the column Q_level into a factor
+    levels = c(1,10,100,500), labels = c("Q = 1", "Q = 10", "Q = 100", "Q = 500"))) %>% 
+  mutate(method = factor(method, ordered = FALSE)) # factor column method 
+# get a new dataset with ratios instead of fitted values
+ratio_data <- fit_data %>% 
+  mutate(f0_hat = f0_hat/f0) %>%
+  mutate(k_hat = k_hat/k) %>%
+  mutate(Aw_hat = Aw_hat/Aw) %>%
+  mutate(Q_hat = ifelse(Q_level == "Q = 1", Q_hat/Q_vec[1], Q_hat)) %>%
+  mutate(Q_hat = ifelse(Q_level == "Q = 10", Q_hat/Q_vec[2], Q_hat)) %>%
+  mutate(Q_hat = ifelse(Q_level == "Q = 100", Q_hat/Q_vec[3], Q_hat)) %>%
+  mutate(Q_hat = ifelse(Q_level == "Q = 500", Q_hat/Q_vec[4], Q_hat))
 
-# covnert the estimated parameters to ratios
-# Q = 1
-fit_Q1_lp <- fit_Q1_lp %*% diag(c(1/f0, 1/Q1, 1/k))
-fit_Q1_nls <- fit_Q1_nls %*% diag(c(1/f0, 1/Q1, 1/k))
-fit_Q1_mle <- fit_Q1_mle %*% diag(c(1/f0, 1/Q1, 1/k))
-colnames(fit_Q1_lp) <- c("f0", "Q", "k")
-colnames(fit_Q1_nls) <- c("f0", "Q", "k")
-colnames(fit_Q1_mle) <- c("f0", "Q", "k")
-# Q = 10
-fit_Q10_lp <- fit_Q10_lp %*% diag(c(1/f0, 1/Q10, 1/k))
-fit_Q10_nls <- fit_Q10_nls %*% diag(c(1/f0, 1/Q10, 1/k))
-fit_Q10_mle <- fit_Q10_mle %*% diag(c(1/f0, 1/Q10, 1/k))
-colnames(fit_Q10_lp) <- c("f0", "Q", "k")
-colnames(fit_Q10_nls) <- c("f0", "Q", "k")
-colnames(fit_Q10_mle) <- c("f0", "Q", "k")
-# Q = 100
-fit_Q100_lp <- fit_Q100_lp %*% diag(c(1/f0, 1/Q100, 1/k))
-fit_Q100_nls <- fit_Q100_nls %*% diag(c(1/f0, 1/Q100, 1/k))
-fit_Q100_mle <- fit_Q100_mle %*% diag(c(1/f0, 1/Q100, 1/k))
-colnames(fit_Q100_lp) <- c("f0", "Q", "k")
-colnames(fit_Q100_nls) <- c("f0", "Q", "k")
-colnames(fit_Q100_mle) <- c("f0", "Q", "k")
-# Q = 500
-fit_Q500_lp <- fit_Q500_lp %*% diag(c(1/f0, 1/Q500, 1/k))
-fit_Q500_nls <- fit_Q500_nls %*% diag(c(1/f0, 1/Q500, 1/k))
-fit_Q500_mle <- fit_Q500_mle %*% diag(c(1/f0, 1/Q500, 1/k))
-colnames(fit_Q500_lp) <- c("f0", "Q", "k")
-colnames(fit_Q500_nls) <- c("f0", "Q", "k")
-colnames(fit_Q500_mle) <- c("f0", "Q", "k")
-# merge datasets
-# Q = 1
-fit_Q1_lp <- as_tibble(fit_Q1_lp) %>% add_column(method = "LP")
-fit_Q1_nls <- as_tibble(fit_Q1_nls) %>% add_column(method = "NLS")
-fit_Q1_mle <- as_tibble(fit_Q1_mle) %>% add_column(method = "MLE")
-fit_Q1 <- bind_rows(fit_Q1_lp, fit_Q1_nls, fit_Q1_mle)
-fit_Q1 <- fit_Q1 %>% add_column(level = "Q = 1")
-# Q = 10
-fit_Q10_lp <- as_tibble(fit_Q10_lp) %>% add_column(method = "LP")
-fit_Q10_nls <- as_tibble(fit_Q10_nls) %>% add_column(method = "NLS")
-fit_Q10_mle <- as_tibble(fit_Q10_mle) %>% add_column(method = "MLE")
-fit_Q10 <- bind_rows(fit_Q10_lp, fit_Q10_nls, fit_Q10_mle) %>% 
-  add_column(level = "Q = 10")
-# Q = 100
-fit_Q100_lp <- as_tibble(fit_Q100_lp) %>% add_column(method = "LP")
-fit_Q100_nls <- as_tibble(fit_Q100_nls) %>% add_column(method = "NLS")
-fit_Q100_mle <- as_tibble(fit_Q100_mle) %>% add_column(method = "MLE")
-fit_Q100 <- bind_rows(fit_Q100_lp, fit_Q100_nls, fit_Q100_mle) %>% 
-  add_column(level = "Q = 100")
-# Q = 500
-fit_Q500_lp <- as_tibble(fit_Q500_lp) %>% add_column(method = "LP")
-fit_Q500_nls <- as_tibble(fit_Q500_nls) %>% add_column(method = "NLS")
-fit_Q500_mle <- as_tibble(fit_Q500_mle) %>% add_column(method = "MLE")
-fit_Q500 <- bind_rows(fit_Q500_lp, fit_Q500_nls, fit_Q500_mle) %>% 
-  add_column(level = "Q = 500")
-# combine all the datasets together
-fit <- bind_rows(fit_Q1, fit_Q10, fit_Q100, fit_Q500)
+# # covnert the estimated parameters to ratios
+# # Q = 1
+# fit_Q1_lp <- fit_Q1_lp %*% diag(c(1/f0, 1/Q1, 1/k))
+# fit_Q1_nls <- fit_Q1_nls %*% diag(c(1/f0, 1/Q1, 1/k))
+# fit_Q1_mle <- fit_Q1_mle %*% diag(c(1/f0, 1/Q1, 1/k))
+# colnames(fit_Q1_lp) <- c("f0", "Q", "k")
+# colnames(fit_Q1_nls) <- c("f0", "Q", "k")
+# colnames(fit_Q1_mle) <- c("f0", "Q", "k")
+# # Q = 10
+# fit_Q10_lp <- fit_Q10_lp %*% diag(c(1/f0, 1/Q10, 1/k))
+# fit_Q10_nls <- fit_Q10_nls %*% diag(c(1/f0, 1/Q10, 1/k))
+# fit_Q10_mle <- fit_Q10_mle %*% diag(c(1/f0, 1/Q10, 1/k))
+# colnames(fit_Q10_lp) <- c("f0", "Q", "k")
+# colnames(fit_Q10_nls) <- c("f0", "Q", "k")
+# colnames(fit_Q10_mle) <- c("f0", "Q", "k")
+# # Q = 100
+# fit_Q100_lp <- fit_Q100_lp %*% diag(c(1/f0, 1/Q100, 1/k))
+# fit_Q100_nls <- fit_Q100_nls %*% diag(c(1/f0, 1/Q100, 1/k))
+# fit_Q100_mle <- fit_Q100_mle %*% diag(c(1/f0, 1/Q100, 1/k))
+# colnames(fit_Q100_lp) <- c("f0", "Q", "k")
+# colnames(fit_Q100_nls) <- c("f0", "Q", "k")
+# colnames(fit_Q100_mle) <- c("f0", "Q", "k")
+# # Q = 500
+# fit_Q500_lp <- fit_Q500_lp %*% diag(c(1/f0, 1/Q500, 1/k))
+# fit_Q500_nls <- fit_Q500_nls %*% diag(c(1/f0, 1/Q500, 1/k))
+# fit_Q500_mle <- fit_Q500_mle %*% diag(c(1/f0, 1/Q500, 1/k))
+# colnames(fit_Q500_lp) <- c("f0", "Q", "k")
+# colnames(fit_Q500_nls) <- c("f0", "Q", "k")
+# colnames(fit_Q500_mle) <- c("f0", "Q", "k")
+# # merge datasets
+# # Q = 1
+# fit_Q1_lp <- as_tibble(fit_Q1_lp) %>% add_column(method = "LP")
+# fit_Q1_nls <- as_tibble(fit_Q1_nls) %>% add_column(method = "NLS")
+# fit_Q1_mle <- as_tibble(fit_Q1_mle) %>% add_column(method = "MLE")
+# fit_Q1 <- bind_rows(fit_Q1_lp, fit_Q1_nls, fit_Q1_mle)
+# fit_Q1 <- fit_Q1 %>% add_column(level = "Q = 1")
+# # Q = 10
+# fit_Q10_lp <- as_tibble(fit_Q10_lp) %>% add_column(method = "LP")
+# fit_Q10_nls <- as_tibble(fit_Q10_nls) %>% add_column(method = "NLS")
+# fit_Q10_mle <- as_tibble(fit_Q10_mle) %>% add_column(method = "MLE")
+# fit_Q10 <- bind_rows(fit_Q10_lp, fit_Q10_nls, fit_Q10_mle) %>% 
+#   add_column(level = "Q = 10")
+# # Q = 100
+# fit_Q100_lp <- as_tibble(fit_Q100_lp) %>% add_column(method = "LP")
+# fit_Q100_nls <- as_tibble(fit_Q100_nls) %>% add_column(method = "NLS")
+# fit_Q100_mle <- as_tibble(fit_Q100_mle) %>% add_column(method = "MLE")
+# fit_Q100 <- bind_rows(fit_Q100_lp, fit_Q100_nls, fit_Q100_mle) %>% 
+#   add_column(level = "Q = 100")
+# # Q = 500
+# fit_Q500_lp <- as_tibble(fit_Q500_lp) %>% add_column(method = "LP")
+# fit_Q500_nls <- as_tibble(fit_Q500_nls) %>% add_column(method = "NLS")
+# fit_Q500_mle <- as_tibble(fit_Q500_mle) %>% add_column(method = "MLE")
+# fit_Q500 <- bind_rows(fit_Q500_lp, fit_Q500_nls, fit_Q500_mle) %>% 
+#   add_column(level = "Q = 500")
+# # combine all the datasets together
+# fit <- bind_rows(fit_Q1, fit_Q10, fit_Q100, fit_Q500)
 # boxplot
 # Q_hat / Q
-ggplot(fit, aes(x = level, y = Q, fill = method)) + geom_boxplot()
+ggplot(ratio_data, aes(x = Q_level, y = Q_hat, fill = method)) + geom_boxplot()
 # k_hat / k
-ggplot(fit, aes(x = level, y = k, fill = method)) + geom_boxplot()
+ggplot(ratio_data, aes(x = Q_level, y = k_hat, fill = method)) + geom_boxplot()
 # f0_hat / f0
-ggplot(fit, aes(x = level, y = f0, fill = method)) + geom_boxplot()
+ggplot(ratio_data, aes(x = Q_level, y = f0_hat, fill = method)) + geom_boxplot()
+
 
