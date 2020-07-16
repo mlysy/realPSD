f8599cbc62680ed115fb5d212d63946f42f98c2a
feiyuzhu <feiyuzhu@users.noreply.github.com>
2020-07-16 00:46:11 -0400

    updated fou model example and the vignette

--- tests/testthat/fou.cpp
+++ tests/testthat/fou.cpp
@@ -3,6 +3,8 @@
 /// @brief fractional OU model, passing data to TMB
 
 #include <TMB.hpp>
+// #include "realPSD/FitMethods.hpp"
+#include "../../inst/include/realPSD/FitMethods.hpp"
 
 namespace fou {
   /// create `matrix<Type>` of zeros.
@@ -30,6 +32,7 @@ namespace fou {
     typedef const Eigen::Ref <const MatrixXd_t> cRefMatrix_t;
     // internal variables
     int N_; ///> problem dimensions
+    matrix<Type> f_; ///>column vector of frequencies
     matrix<Type> f2_; ///> column vector of squared frequencies.
   public:
     /// Constructor.
@@ -37,18 +40,21 @@ namespace fou {
     /// Set frequency vector.
     void set_f(cRefMatrix_t& f);
     /// Evaluate the normalized PSD.
-    void eval(RefMatrix_t U, cRefMatrix_t& phi, cRefMatrix_t& f);
+    void eval(RefMatrix_t U, cRefMatrix_t& phi);
   };
 
   template<class Type>
   inline UFun<Type>::UFun(int N) {
     N_ = N;
+    f_ = zero_matrix<Type>(N_, 1);
     f2_ = zero_matrix<Type>(N_,1);
   }
 
   template<class Type>
   inline void UFun<Type>::set_f(cRefMatrix_t& f) {
     N_ = f.size();
+    f_ = zero_matrix<Type>(N_,1);
+    f_ = f;
     f2_ = zero_matrix<Type>(N_,1);
     f2_ = f.cwiseProduct(f);
     return;
@@ -57,9 +63,9 @@ namespace fou {
   /// Parameters are: `phi = (H, gamma)`
   // psd = |f|^(1-2H) / (f^2 + gamma^2)
   template <class Type>
-  inline void UFun<Type>::eval(RefMatrix_t U, cRefMatrix_t& phi, cRefMatrix_t& f) {
+  inline void UFun<Type>::eval(RefMatrix_t U, cRefMatrix_t& phi) {
     U = f2_.array() + phi(1,0) * phi(1,0);
-    U = (f.array().abs()).pow(1-Type(2.0)*phi(0,0)) / U.array(); // use ArrayBase::pow() and ::abs() functions
+    U = (f_.array().abs()).pow(1-Type(2.0)*phi(0,0)) / U.array(); // use ArrayBase::pow() and ::abs() functions
     return;
   }
 
@@ -74,33 +80,32 @@ namespace fou {
   #define TMB_OBJECTIVE_PTR this
 } // end namespace fou
 
-
-#undef TMB_OBJECTIVE_PTR
-#define TMB_OBJECTIVE_PTR obj
-template<class Type, class UFun>
-Type FitMethods(objective_function<Type>* obj,
-    UFun (*make_Ufun)(int, objective_function<Type>*)) {
-  // data
-  DATA_MATRIX(f);
-  // parameters
-  PARAMETER_MATRIX(phi);
-  // calculate U
-  int N = f.size();
-  UFun Ufun = make_Ufun(N, obj);
-  Ufun.set_f(f);
-  SIMULATE {
-    matrix<Type> U(N,1);
-    Ufun.eval(U, phi, f);
-    REPORT(U);
-  }
-  return Type(0.0);
-}
-#undef TMB_OBJECTIVE_PTR
-#define TMB_OBJECTIVE_PTR this
+// #undef TMB_OBJECTIVE_PTR
+// #define TMB_OBJECTIVE_PTR obj
+// template<class Type, class UFun>
+// Type FitMethods(objective_function<Type>* obj,
+//     UFun (*make_Ufun)(int, objective_function<Type>*)) {
+//   // data
+//   DATA_MATRIX(f);
+//   // parameters
+//   PARAMETER_MATRIX(phi);
+//   // calculate U
+//   int N = f.size();
+//   UFun Ufun = make_Ufun(N, obj);
+//   Ufun.set_f(f);
+//   SIMULATE {
+//     matrix<Type> U(N,1);
+//     Ufun.eval(U, phi, f);
+//     REPORT(U);
+//   }
+//   return Type(0.0);
+// }
+// #undef TMB_OBJECTIVE_PTR
+// #define TMB_OBJECTIVE_PTR this
 
 template<class Type>
 Type objective_function<Type>::operator() () {
   using namespace fou;
-  return FitMethods<Type, UFun<Type> >(this, make_Ufun<Type>);
+  return realPSD::FitMethods<Type, UFun<Type> >(this, make_Ufun<Type>);
 }
 
