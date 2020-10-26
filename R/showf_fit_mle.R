aaff2230809d10bceb80508426a4a8c8f93219c4
feiyuzhu <feiyuzhu@users.noreply.github.com>
2020-10-25 22:22:32 -0400

    updated SHOWF model MLE method

--- R/showf_fit_mle.R
+++ R/showf_fit_mle.R
@@ -8,7 +8,7 @@
 #' @param bin_type Either "mean" or "median".  The former is more efficient, the latter is more robust, so better for preliminary SHOW fit to denoise against.
 #' @param fit_type If "direct", fit all parameters at once.  If "incremental", do one, then two, then three, etc.
 #' @param optimizer Either "optim" (in R) or "Adam" (supplied by realPSD). For now, Adam's hyperparameter tunning is not fully supported. Learning rate and nsteps are preset. `Adam` is only used for `direct` fitting.
-#' @param getHessian If TRUE, return the numerical Hessian matrix of the original SHOW parameter f0, Q, Rw
+#' @param vcov If TRUE, return the numerical Hessian matrix of the original SHOF parameter k, f0, Q, Af
 #' @param ... Additional arguments to [stats::optim()].
 #' 
 #' @return A list with elements 
@@ -16,13 +16,13 @@
 #'   \item{`par`}{The fitted parameter value which is the minimizer of the objective function.}
 #'   \item{`Temp`}{The temperature constant used in calculation, Kelvin.}
 #'   \item{`value`}{The value of the objective function (negative loglikelihood) at the given `par`.}
-#'   \item{`he`}{The numerical Hessian matrix of the SHOW model parameter f0, Q, Rw, Rf, alpha.}
+#'   \item{`cov`}{The numerical variance-covariance matrix for all the parameters (original scale).}
 #'   \item{`exitflag`}{The exit flag given by the optimizer, for debug purposes}
 #' }
 showf_fit_mle <- function(fseq, Ypsd, fs, Temp, phi0,
                         fit_type = c("direct", "incremental"),
                         optimizer = c("optim", "Adam"),
-                        getHessian = FALSE, ...) {
+                        vcov = FALSE, ...) {
   fit_type <- match.arg(fit_type)
   optimizer <- match.arg(optimizer)
   constY <- mean(Ypsd) # normalize to avoid numerical overflow
@@ -88,11 +88,12 @@ showf_fit_mle <- function(fseq, Ypsd, fs, Temp, phi0,
   }
   # construct final estimator
   theta <- c(exp(phi), tau = obj$simulate(phi)$tau * constY)
+  theta <- setNames(theta, nm = c("f0", "Q", "Rw", "Rf", "alpha", "tau"))
   # cov
   # cov <- NULL
   he <- NULL
-  if(getHessian) {    
-    obj_nll <- TMB::MakeADFun(data = list(model = "SHOWF_nat",
+  if(vcov) {    
+    obj_nll <- TMB::MakeADFun(data = list(model = "SHOWF_log",
                                     method = "MLE_nll",
                                     f = as.matrix(fseq),
                                     Y = as.matrix(Ypsd/constY),
@@ -100,16 +101,15 @@ showf_fit_mle <- function(fseq, Ypsd, fs, Temp, phi0,
                         parameters = list(phi = as.matrix(rep(0,5)), tau = 0),
                         map = map,
                         silent = TRUE, DLL = "realPSD_TMBExports")
-    phi_tau <- c(exp(phi), tau = obj$simulate(phi)$tau)
-    he <- numDeriv::hessian(func = obj_nll$fn, x = phi_tau)
-    # browser()
-    # ind <- c(1,2,4,5)
-    # tmp <- he[ind, ind]
-    # solve(tmp)
-    he <- he[c(1,2,5), c(1,2,5)] # the hessian matrix will be singular if Rf and alpha are both present
-    # cov <- solve(he)
+    par_opt <- get_par_showf(theta, Temp)
+    he <- numDeriv::hessian(func = function(par) {
+      phi_zeta <- get_phi(par, Temp = Temp, method = "MLE", model = "SHOWF", const = constY)
+      obj_nll$fn(phi_zeta)
+    }, x = par_opt)
+    he <- he[c(1:3,6), c(1:3,6)] # similar to LP method
+    cov <- chol2inv(chol(he))
   }
   list(par = get_par_showf(theta, Temp = Temp),
-       value = obj$fn(phi), he = he,
+       value = obj$fn(phi), cov = cov,
        exitflag = exitflag)
 }
